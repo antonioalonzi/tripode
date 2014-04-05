7bb6ebc9877b6e0c0cc969ce5fbaca9203ef4236
antonioalonzi <antonioalonzi85@gmail.com>
2014-04-05 15:39:33 +0100

    Completed DispatcherService Tests

--- test/DispatcherServiceTest.php
+++ test/DispatcherServiceTest.php
@@ -3,7 +3,7 @@ require_once("model/Context.php");
 require_once("test/FileSystemMockAdaptor.php");
 
 class DispatcherServiceTest extends PHPUnit_Framework_TestCase {
-
+	
 	private $dispatcherService;
 	private $fileSystemMockAdaptor;
 	
@@ -46,11 +46,43 @@ class DispatcherServiceTest extends PHPUnit_Framework_TestCase {
 	}
 	
 	public function testDispatchExecutesDoGetOnAnActionIfDefined() {
+		// Given
+		$_REQUEST['action'] = 'test';
+		$_SERVER['REQUEST_METHOD'] = "GET";
 		
+		// TestAction defined at the end of the file
+		
+		$this->fileSystemMockAdaptor->stateThatFileExists('actions/TestAction.php');
+		$this->fileSystemMockAdaptor->stateThatFileExists('pages/home.php');
+		$this->fileSystemMockAdaptor->build();
+		
+		// when
+		$this->dispatcherService->dispatch();
+		
+		// Assert
+		$this->assertEquals('home', $_REQUEST['PAGE']);
+		$this->assertEquals('pages/home.php', $_REQUEST['PAGE_FILE']);
+		$this->assertEquals('TestAction.doGet()', $_REQUEST['MESSAGE']);
 	}
 	
 	public function testDispatchExecutesDoPostOnAnActionIfDefined() {
+		// Given
+		$_REQUEST['action'] = 'test';
+		$_SERVER['REQUEST_METHOD'] = "POST";
 		
+		// TestAction defined at the end of the file
+		
+		$this->fileSystemMockAdaptor->stateThatFileExists('actions/TestAction.php');
+		$this->fileSystemMockAdaptor->stateThatFileExists('pages/post.php');
+		$this->fileSystemMockAdaptor->build();
+		
+		// when
+		$this->dispatcherService->dispatch();
+		
+		// Assert
+		$this->assertEquals('post', $_REQUEST['PAGE']);
+		$this->assertEquals('pages/post.php', $_REQUEST['PAGE_FILE']);
+		$this->assertEquals('TestAction.doPost()', $_REQUEST['MESSAGE']);
 	}
 	
 	public function testDispatchShowsTheHomePageWithAnErrorIfActionNotValid() {
@@ -68,6 +100,17 @@ class DispatcherServiceTest extends PHPUnit_Framework_TestCase {
 		$this->assertEquals('home', $_REQUEST['PAGE']);
 		$this->assertEquals('pages/home.php', $_REQUEST['PAGE_FILE']);
 		$this->assertEquals('Action InexistentAction does not exist.', $_REQUEST['ERROR']);
+	}
+}
+
+class TestAction {
+	function doGet() {
+		$_REQUEST['MESSAGE'] = 'TestAction.doGet()';
+	}
+	
+	function doPost() {
+		$_REQUEST['MESSAGE'] = 'TestAction.doPost()';
+		$_REQUEST['PAGE'] = 'post';
 	}
 }
 ?>
