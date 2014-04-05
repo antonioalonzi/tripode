353d67696e7100ac4172927cca4848937ec18f26
antonioalonzi <antonioalonzi85@gmail.com>
2014-04-05 16:53:37 +0100

    Modified icons.

--- pages/galleryCategoriesEdit.php
+++ pages/galleryCategoriesEdit.php
@@ -3,10 +3,30 @@
 	<form action="?action=gallery" method="post" class="form-horizontal">
 		<table class="table table-striped table-bordered">
 			<tbody>
-				<?php foreach (Context::getInstance()->galleryService->getGalleryCategories(true) as $categoryItem) { ?>
+				<?php $i = 0 ?>
+				<?php $categories = Context::getInstance()->galleryService->getGalleryCategories(true) ?>
+				<?php foreach ($categories as $categoryItem) { ?>
 				<tr>
 					<td><?= $categoryItem->getName() ?></td>
 					<td>
+						<!-- Up/Down Icons -->
+						<?php if ($i > 0 && $categoryItem->getPosition() > 0) { ?>
+							<a class="methodPost" href="?action=gallery&galleryAction=upCategory&category=<?= $categoryItem->getFileName() ?>" title="<?= Context::getInstance()->translationService->translate("gallery.actionUp") ?>">
+								<img alt="<?= Context::getInstance()->translationService->translate("gallery.actionUp") ?>" src="img/icons/up.png" />
+							</a>
+						<?php } else {?>
+							<img src="img/icons/empty.png" />
+						<?php }?>
+						
+						<?php if ($i < count($categories)-1) { ?>
+							<a class="methodPost" href="?action=gallery&galleryAction=downCategory&category=<?= $categoryItem->getFileName() ?>" title="<?= Context::getInstance()->translationService->translate("gallery.actionDown") ?>">
+								<img alt="<?= Context::getInstance()->translationService->translate("gallery.actionDown") ?>" src="img/icons/down.png" />
+							</a>
+						<?php } else {?>
+							<img src="img/icons/empty.png" />
+						<?php }?>
+						
+						<!-- Hide/Show Icon -->
 						<?php if (!$categoryItem->isHidden()) { ?>
 							<a class="methodPost" href="?action=gallery&galleryAction=hideCategory&category=<?= $categoryItem->getFileName() ?>" title="<?= Context::getInstance()->translationService->translate("gallery.actionHide") ?>">
 								<img alt="<?= Context::getInstance()->translationService->translate("gallery.actionHide") ?>" src="img/icons/hide.png" />
@@ -17,6 +37,7 @@
 							</a>
 						<?php } ?>
 					</td>
+					<?php $i++ ?>
 				</tr>
 				<?php } ?>
 			</tbody>
