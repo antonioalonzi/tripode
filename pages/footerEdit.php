d67437064125bb2fb36d719b79a4e4120b028b20
antonioalonzi <antonioalonzi85@gmail.com>
2014-06-29 10:37:25 +0100

    #13 added display link to tripode option to the form

--- pages/footerEdit.php
+++ pages/footerEdit.php
@@ -1,19 +1,18 @@
 <div>
 	<p><?= Context::getInstance()->translator->translate("footer.title") ?></p>
 	
-	<form action="?action=footer" method="post" class="form-vertical">
-		<div class="control-group">
+	<form action="?action=footer" method="post">
+		<fieldset class="well">
+			<?= Context::getInstance()->translator->translate("footer.displayLinkToTripode") ?>
+			<select name="linkToTripode">
+				<option value="Yes"><?= Context::getInstance()->translator->translate("general.yes") ?></option>
+				<option value="No"><?= Context::getInstance()->translator->translate("general.no") ?></option>
+			</select>
+			
 			<div class="controls">
-				<label id="text-lbl" for="linkToTripode" class="required"><?= Context::getInstance()->translator->translate("footer.displayLinkToTripode") ?></label>
-				<select name="linkToTripode">
-					<option value="Yes"><?= Context::getInstance()->translator->translate("general.yes") ?></option>
-					<option value="No"><?= Context::getInstance()->translator->translate("general.no") ?></option>
-				</select>
+				<button type="submit" class="btn btn-primary"><?= Context::getInstance()->translator->translate("action.save") ?></button>
 			</div>
-		</div>
-		<div class="controls">
-			<button type="submit" class="btn btn-primary"><?= Context::getInstance()->translator->translate("action.save") ?></button>
-		</div>
+		</fieldset>
 	</form>
 	
 </div>
